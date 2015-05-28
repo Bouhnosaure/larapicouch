842af2a0447ba9fddded5fe16fa2bd4ddcfe70d7
Alan <acorbel@tibanne.com>
2015-05-28 09:50:02 +0200

    Ajout de plant_id sur la table des plantes de l'user

--- database/migrations/2015_05_27_165457_create_table_appdata.php
+++ database/migrations/2015_05_27_165457_create_table_appdata.php
@@ -19,9 +19,17 @@ class CreateTableAppdata extends Migration
             $table->increments('id');
             $table->string('alias', 100)->nullable();
             $table->string('ip', 100)->nullable();
+            $table->integer('plant_id')->unsigned();
             $table->timestamps();
 
         });
+
+        Schema::table('sqlite_app', function($table) {
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+
     }
 
     /**
