e6ee31eadaafbdaab32f11e2477d0143543c072f
Alexandre <alexandre.mangin@viacesi.fr>
2015-05-31 11:50:43 +0200

    valdiation

--- app/Http/Transformers/PlantLocalTransformer.php
+++ app/Http/Transformers/PlantLocalTransformer.php
@@ -8,11 +8,18 @@ class PlantLocalTransformer extends Transformer
     public function transform($obj)
     {
         $array = [
-            'data' => $obj[0],
-            'info' => $obj[1],
-            'photo' => $obj[2]->dataUrl,
-            'current' => $obj[3],
-            'notification' =>$obj[4]
+            'id' => $obj[0]->id,
+            'alias' => $obj[0]->alias,
+            'commonName' => $obj[0]->commonName,
+            'scientificName' => $obj[0]->scientificName,
+            'category' => $obj[0]->familyName,
+            'averageTemp' => $obj[0]->genericInstructionTemperature,
+            'minTemp' => $obj[0]->hardiness,
+            'enlightment' => $obj[0]->genericInstructionLight,
+            'humidity' => $obj[0]->genericInstructionWater,
+            'photo' => $obj[1]->dataUrl,
+            'current' => $obj[2],
+            'notification' =>$obj[3]
         ];
 
         return $array;
